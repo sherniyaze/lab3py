b727ce66ab92701b3e1cd48dd3ee784078f91e73
sherniyaze <sherniyazerkin@gmail.com>
2025-01-30 22:26:04 +0500

    update classtask 3

---
+++ class_task3.py
@@ -0,0 +1,17 @@
+class Shape():
+    def __init__(self):
+        self.length = 0
+        self.width = 0
+    def areas(self):
+        return 0
+
+class Rectangle(Shape):
+    def __init__(self):
+        super().__init__()
+        self.lengthy = int(input())
+        self.widthy = int(input())
+    def areas(self):
+        return self.lengthy*self.widthy
+
+my_fav_rectangle = Rectangle()
+print(my_fav_rectangle.areas())
